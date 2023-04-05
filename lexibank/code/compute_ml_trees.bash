ec6f535e801c17283c2756c03412b60c334d5d96
gerhardJaeger <gerhard.jaeger@uni-tuebingen.de>
2023-04-05 09:39:36 +0200

    renaming best trees

--- lexibank/code/compute_ml_trees.bash
+++ lexibank/code/compute_ml_trees.bash
@@ -13,3 +13,15 @@ do
     db=`basename $f .catg`
     raxml-ng --msa ../data/catg_files/$f --model BIN --prefix ../data/catg_mltrees/$db --prob-msa on --redo
 done
+
+for f in `ls ../data/phylip_mltrees/*bestTree`
+do
+    b=`basename $f .phy.raxml.bestTree`
+    cp $f ../data/phylip_mltrees/$b"_mltree.tre"
+done
+
+for f in `ls ../data/catg_mltrees/*bestTree`
+do
+    b=`basename $f .raxml.bestTree`
+    cp $f ../data/catg_mltrees/$b"_mltree.tre"
+done

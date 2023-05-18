193eef29d357d7873be1cc1dc45aceb175cbd8f4
gerhardJaeger <gerhard.jaeger@uni-tuebingen.de>
2023-05-18 17:35:43 +0200

    some formatting

--- lexibank/code/collect_posterior_trees.r
+++ lexibank/code/collect_posterior_trees.r
@@ -3,9 +3,9 @@ library(stringr)
 library(TAF)
 
 
-contrees <- list.files("mrbayes_scripts/output", pattern="*.con.tre")
+contrees <- list.files("mrbayes_scripts/output", pattern = "*.con.tre")
 
-dbs <- str_split(contrees, ".con.tre", simplify=T)[,1]
+dbs <- str_split(contrees, ".con.tre", simplify = TRUE)[, 1]
 
 mkdir("../data/mrbayes_posteriors/")
 
@@ -13,8 +13,11 @@ mkdir("../data/mrbayes_posteriors/")
 for (db in dbs) {
     trees1 <- read.nexus(str_interp("mrbayes_scripts/output/${db}.run1.t"))
     trees2 <- read.nexus(str_interp("mrbayes_scripts/output/${db}.run2.t"))
-    bi <- round(length(trees1)/4)
+    bi <- round(length(trees1) / 4)
     trees <- c(trees1[-(1:bi)], trees2[-(1:bi)])
     trees <- sample(trees, min(length(trees), 1000))
-    write.tree(trees, str_interp("../data/mrbayes_posteriors/${db}.posterior.tree"))
+    write.tree(
+        trees,
+        str_interp("../data/mrbayes_posteriors/${db}.posterior.tree")
+    )
 }
